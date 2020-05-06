e12f4c987ec5aa4205225d02b05f9f96b0969969
Jakob Kempf <Jakob.Kempf@meiko-global.com>
2020-05-06 08:49:33 +0200

    First test to get run C# WPF on Raspberry Pi

---
+++ machine_communication/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace machine_communication
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void my_bt_stream_Click(object sender, RoutedEventArgs e)
+        {
+            if (my_eingabe_ip != null)
+            {
+                string ip_eingabe = my_eingabe_ip.Text;
+                my_tb_test.Text = "Die von Ihnen eingegebene IP-Adresse lautet:\n" + ip_eingabe;
+
+            }
+        }
+    }
+}
